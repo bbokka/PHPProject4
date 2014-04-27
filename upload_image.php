4120efdbe17e53d03b7a3e6c8e750c471347f6bb
unknown <bbokka@D-1103C-3.cs.odu.edu>
2014-04-26 21:24:54 -0400

    userprofile

--- upload_image.php
+++ upload_image.php
@@ -1,12 +1,42 @@
 <?php
-	if ($_FILES["file"]["error"] > 0) {
-		echo "Error: " . $_FILES["file"]["error"] . "<br>";
+	//creating an array to check for the extension
+	$allowedExts = array("gif", "jpeg", "jpg", "png");
+	//extracting the extension of the file given by the user explode() function 
+	$temp = explode(".", $_FILES["file"]["name"]);
+	$extension = end($temp);
+
+	if ((($_FILES["file"]["type"] == "image/gif")
+	|| ($_FILES["file"]["type"] == "image/jpeg")
+	|| ($_FILES["file"]["type"] == "image/jpg")
+	|| ($_FILES["file"]["type"] == "image/pjpeg")
+	|| ($_FILES["file"]["type"] == "image/x-png")
+	|| ($_FILES["file"]["type"] == "image/png"))
+	&& ($_FILES["file"]["size"] < 20000)
+	&& in_array($extension, $allowedExts)) {
+		if ($_FILES["file"]["error"] > 0) {
+			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+		}
+		else
+		{
+			echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+			echo "Type: " . $_FILES["file"]["type"] . "<br>";
+			echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+			echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+		if (file_exists("images/" . $_FILES["file"]["name"])) 
+		{
+			//javascript for this error
+			echo $_FILES["file"]["name"] . " already exists. ";
+		} 
+		else
+		{
+			move_uploaded_file($_FILES["file"]["tmp_name"],"images/" . $_FILES["file"]["name"]);
+			
+			echo "Stored in: " . "images/" . $_FILES["file"]["name"];
+		}
+	  }
 	} 
 	else 
 	{
-		echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-		echo "Type: " . $_FILES["file"]["type"] . "<br>";
-		echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-		echo "Stored in: " . $_FILES["file"]["tmp_name"];
+		echo "Invalid file";
 	}
 ?>
